ea48176c4ff8842732978fd8956543ebc109fb55
KingRovs771 <rizkybudiarto890@gmail.com>
2024-01-01 16:20:46 +0700

    Done Home

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -7,27 +7,24 @@ export default function Home() {
     <div className="w-full min-h-screen bg-white flex flex-row">
       <div className="flex-1 bg-white">
         <div className="flex flex-row">
-          <div className="max-w-sm rounded overflow-hidden shadow-lg mt-9 ml-3">
+          <div className="w-96 rounded overflow-hidden shadow-lg mt-9 ml-3">
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
               <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
             </div>
           </div>
           {/* Card 1*/}
-          <div className="max-w-sm rounded overflow-hidden shadow-lg mt-9 ml-3">
+          <div className="w-64 rounded overflow-hidden shadow-lg mt-9 ml-3">
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
               <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+          </div>
+          {/* Card 1*/}
+          <div className="w-64 rounded overflow-hidden shadow-lg mt-9 ml-3">
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
             </div>
           </div>
         </div>
